2932ea713a7889a93dd8ef37e65932c75bdff92b
Harshavi Shah <harshavi-shah@outlook.com>
2023-11-28 11:27:16 -0500

    Initial commit

---
+++ If_Elseif.py
@@ -0,0 +1,11 @@
+Grades = int(input("Enetr your grades "))
+if Grades > 90:
+    print("A")
+elif Grades > 80:
+    print("B")
+elif Grades > 70:
+    print("C")
+elif Grades > 60:
+    print("D")
+else:
+    print("Fail")
---
+++ TempConvert.py
@@ -0,0 +1,3 @@
+F= int(input("Enetr Temp in Fehrenheit "))
+C = (F-32)/1.8
+print("Temp in Celsius is ",C)
